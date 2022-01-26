ebebbae466429a33ff6a991475a80fad9a1e65c9
Mohamed Abdessamed Rezazi <ma.rezazi@esi-sba.dz>
2022-01-26 23:38:36 +0100

    added projectiles movements

--- index.js
+++ index.js
@@ -38,21 +38,42 @@ class Projectile {
     ctx.fillStyle = this.color;
     ctx.fill();
   }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
 }
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
 
+const projectiles = [];
+
+function animate() {
+  // loop function, for example we can use it in order to fires can move from cordonate to other.
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // to clear the colisions between projectiles; 0,0 : means start from x0, y0, canvas.width, canvas.height: clear the whole canvas.
+  player.draw();
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
+// to move the projectile from the center to the mouse clicked, we need to add vilocity
 window.addEventListener('click', (event) => {
-  const projectile = new Projectile(
-    event.clientX,
-    event.clientY,
-    5,
-    'red',
-    null
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
   );
-  projectile.draw();
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+  );
 });
+
+animate();
